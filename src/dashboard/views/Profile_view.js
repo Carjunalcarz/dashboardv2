27ea362ec533fe7709209cd6c5e4859958debf4d
ajncarz <cardona.aljun.csucc@gmail.com>
2024-04-20 17:07:15 +0800

    Commit message

---
+++ src/dashboard/views/Profile_view.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import Navbar from "../components/Navbar.js";
+import Sidebar from "../components/Sidebar.js";
+
+import Profile from "../profile/src/views/Profile.js";
+
+export default function Profile_view() {
+  return (
+    <>
+      {/* <Sidebar /> */}
+      <div className="relative md:ml-64 bg-blueGray-100">
+        <Navbar />
+        {/* Header */}
+        <Profile />
+
+        <footer className="block py-4">
+          <div className="container mx-auto px-4">
+            <hr className="mb-4 border-b-1 border-blueGray-200" />
+            <div className="flex flex-wrap items-center md:justify-between justify-center">
+              <div className="w-full md:w-4/12 px-4">
+                <div className="text-sm text-blueGray-500 font-semibold py-1">
+                  Copyright © {new Date().getFullYear()}{" "}
+                  <a
+                    href="https://www.creative-tim.com"
+                    className="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
+                  >
+                    Creative Tim
+                  </a>
+                </div>
+              </div>
+              <div className="w-full md:w-8/12 px-4">
+                <ul className="flex flex-wrap list-none md:justify-end  justify-center">
+                  <li>
+                    <a
+                      href="https://www.creative-tim.com"
+                      className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                    >
+                      Creative Tim
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.creative-tim.com/presentation"
+                      className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                    >
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="http://blog.creative-tim.com"
+                      className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/creativetimofficial/tailwind-starter-kit/blob/main/LICENSE.md"
+                      className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                    >
+                      MIT License
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
